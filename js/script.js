97700be2d50fba3e4ba6de5cd845f4d6f9cf78d4
heimse <heimseweb@gmail.com>
2021-10-10 22:29:51 +0300

    Tabs and footer were done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-window.addEventListener('load', function () {
+window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     //burger
     
-    const burgerTrigger = this.document.querySelector('.burger-menu'),
-          body = this.document.querySelector('body');
+    const burgerTrigger = document.querySelector('.burger-menu'),
+          body = document.querySelector('body'),
+          burgerBody = document.querySelector('.burger__body');
 
     burgerTrigger.addEventListener('click', () => {
         burgerTrigger.classList.toggle('_active');
+        burgerBody.classList.toggle('_active');
         body.classList.toggle('_lock');
     });
 
     //popup
     const popup = this.document.querySelector('.popup');
     //!Change trigger in real project
-    burgerTrigger.addEventListener('click', () =>{
-        popup.classList.toggle('_visible');
-    });
+    // .addEventListener('click', () =>{
+    //     popup.classList.toggle('_visible');
+    // });
+
+
+    //s
 });
