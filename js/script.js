884a6f22c2c042f2a3ed8998f695e23eb6fcb932
heimse <heimseweb@gmail.com>
2021-10-11 18:21:13 +0300

    Added backgrounds via js function setBackground

--- js/script.js
+++ js/script.js
@@ -20,6 +20,44 @@ window.addEventListener('DOMContentLoaded', () => {
     //     popup.classList.toggle('_visible');
     // });
 
+    initilizeBackgrounds();
 
-    //s
+    window.addEventListener('resize', () => {
+        initilizeBackgrounds();
+    });
+
+    function setBackground(background, top, left, height, width, backgroundColor) {
+        background.style.cssText = `
+        position: absolute;
+        top: ${top + 'px'};
+        left: ${left + 'px'};
+        height: ${height + 'px'};
+        width: ${width + 'px'};
+        background-color: ${backgroundColor};
+        z-index: -1;
+        `;
+    }
+    
+    function initilizeBackgrounds() {
+        console.log(window.innerWidth);
+        if(window.innerWidth > 914) {
+            const tabsBackground = document.querySelector('.tabs__background'),
+                  offerBackground = document.querySelector('.offer__background');
+    
+            const header = document.querySelector('.header'),
+                  tabs = document.querySelector('.tabs'),
+                  offer = document.querySelector('.offer');
+    
+            const tabsBackgroundHeight = header.offsetHeight + tabs.offsetHeight + 30,
+                  tabsBackgroundWidth = window.innerWidth / 2,
+                  tabsBackgroundRight = window.innerWidth / 2;
+    
+            const offerBackgroundTop = tabsBackgroundHeight,
+                  offerBackgroundWidth = window.innerWidth / 2,
+                  offerBackgroundHeight = offer.offsetHeight;
+
+            setBackground(tabsBackground, 0, tabsBackgroundRight, tabsBackgroundHeight, tabsBackgroundWidth, '#effdff');
+            setBackground(offerBackground, offerBackgroundTop, 0, offerBackgroundHeight, offerBackgroundWidth, '#fafff0');
+        }
+    }
 });
