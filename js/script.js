b0a7ac464a9c8a1151f986da66525138d1acf74a
heimse <heimseweb@gmail.com>
2021-10-12 19:21:57 +0300

    Cleared console logs & added fade-in animations to the tabs

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     //burger
-    
+
     const burgerTrigger = document.querySelector('.burger-menu'),
-          body = document.querySelector('body'),
-          burgerBody = document.querySelector('.burger__body');
+        body = document.querySelector('body'),
+        burgerBody = document.querySelector('.burger__body');
 
     burgerTrigger.addEventListener('click', () => {
         burgerTrigger.classList.toggle('_active');
@@ -23,36 +23,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //tabs
     const tabsContainerWidth = document.querySelector('.tabs__content__container').offsetWidth,
-    tabsContainerWHeight = document.querySelector('.tabs__content__container').offsetHeight;
+        tabsContainerWHeight = document.querySelector('.tabs__content__container').offsetHeight;
 
-    const tabsImgs = document.querySelectorAll('.tabs__image');
-
-    setImagesSize(tabsImgs, tabsContainerWidth, tabsContainerWHeight);
-
-
-
-    function setImagesSize(images, width, height) {
-    
-    images.forEach((item) => {
-        console.log(images);
-        console.log(width);
-        console.log(height);
-        item.style.width = width;
-        item.style.height = height;
-    });
-    }
 
     const tabsPickers = document.querySelectorAll('.tabs__picker'),
-          tabsContent = document.querySelectorAll('.tabs__content');
+        tabsContent = document.querySelectorAll('.tabs__content');
 
     let tabCounter = 0;
 
     hideTabs(tabsContent);
     showTab(tabsContent, tabsPickers);
 
-    for(let i = 0; i < tabsContent.length; i++) {
+    for (let i = 0; i < tabsContent.length; i++) {
         tabsPickers[i].addEventListener('click', () => {
-            console.log('FDLKJSHF');
             tabCounter = i;
             hideTabs(tabsContent);
             showTab(tabsContent, tabsPickers);
@@ -62,15 +45,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function showTab(tabs, tabsPicker) {
         tabs[tabCounter].classList.remove('_hidden');
-
-
+        tabs[tabCounter].classList.add('_fade-in');
 
         tabsPicker.forEach(item => {
             item.classList.remove('_active');
         });
 
         tabsPicker[tabCounter].classList.add('_active');
-        
     }
 
     function hideTabs(tabs) {
@@ -79,7 +60,7 @@ window.addEventListener('DOMContentLoaded', () => {
             item.classList.add('_hidden');
         });
     }
-    
+
 
 
 
@@ -105,21 +86,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     function initilizeBackgrounds() {
-        if(window.innerWidth > 914) {
+        if (window.innerWidth > 914) {
             const tabsBackground = document.querySelector('.tabs__background'),
-                  offerBackground = document.querySelector('.offer__background');
-    
+                offerBackground = document.querySelector('.offer__background');
+
             const header = document.querySelector('.header'),
-                  tabs = document.querySelector('.tabs'),
-                  offer = document.querySelector('.offer');
-    
+                tabs = document.querySelector('.tabs'),
+                offer = document.querySelector('.offer');
+
             const tabsBackgroundHeight = header.offsetHeight + tabs.offsetHeight + 20,
-                  tabsBackgroundWidth = window.innerWidth / 2,
-                  tabsBackgroundRight = window.innerWidth / 2 - 15;
-    
+                tabsBackgroundWidth = window.innerWidth / 2,
+                tabsBackgroundRight = window.innerWidth / 2 - 15;
+
             const offerBackgroundTop = tabsBackgroundHeight + 25,
-                  offerBackgroundWidth = window.innerWidth / 2,
-                  offerBackgroundHeight = offer.offsetHeight;
+                offerBackgroundWidth = window.innerWidth / 2,
+                offerBackgroundHeight = offer.offsetHeight;
 
             setBackground(tabsBackground, 0, tabsBackgroundRight, tabsBackgroundHeight, tabsBackgroundWidth, '#effdff');
             setBackground(offerBackground, offerBackgroundTop, 0, offerBackgroundHeight, offerBackgroundWidth, '#fafff0');
